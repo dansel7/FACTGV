775a7adca5019363de8732b3a9b30cad825cfcdf
dansel7 <dansel7@hotmail.com>
2013-07-18 13:17:24 -0600

    Creacion

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body >
+        <img src="images/fondo.png" style="width: 100%;height: 100%;position: absolute">
+       <center><div style="position:relative;"> LOGIN</div></center>
+       
+    </body>
+</html>
